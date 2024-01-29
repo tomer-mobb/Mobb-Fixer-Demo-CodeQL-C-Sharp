d5ee379fb22e7f6c71a03d6a7797bc1838488cef
tomer-mobb <132216976+tomer-mobb@users.noreply.github.com>
2024-01-29 16:37:22 +0700

    Update program.cs

--- src/program.cs
+++ src/program.cs
@@ -26,17 +26,17 @@ public class Program
             HttpListenerRequest req = ctx.Request;
             var var1 = req.QueryString["var1"];
             var var2 = req.QueryString["var2"];
-            Trace.TraceInformation(var1);
-            Trace.TraceInformation("test" + var2);
-            Trace.TraceInformation("test" + var2 + "test" + var1);
-            Trace.TraceInformation("Test, {0}", var2);
+            Trace.TraceInformation(var1); 
+            Trace.TraceInformation("test" + var2); 
+            Trace.TraceInformation("test" + var2 + "test" + var1); 
+            Trace.TraceInformation("Test, {0}", var2); 
 
             string val = (string)req.QueryString["val"];
             try {
                 int value = Int32.Parse(val);
             }
                 catch (FormatException fe) {
-                log.LogError("Failed to parse val= " + val);
+                log.LogError("Failed to parse val= " + val); 
             }
         }
     }
